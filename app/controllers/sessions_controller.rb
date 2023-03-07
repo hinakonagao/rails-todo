df8eaed3c9079c1591fe873e035fec1f05baa0df
hinakonagao <11hinako11@gmail.com>
2023-03-07 10:23:57 +0900

    コメント追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # ログイン処理
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])

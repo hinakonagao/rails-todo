e43985a3ceb142f7423b1218ad262115de136ce5
hinakonagao <11hinako11@gmail.com>
2023-03-19 19:45:30 +0900

    タスクの新規作成・編集・削除は正しいユーザーでないと行えないよう制御

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   require 'debug'
 
-  before_action :logged_in_user, only: %i[index new create edit update] # ログイン済みユーザーかどうか確認
-  before_action :correct_user, only: %i[create update] # 正しいユーザーかどうか確認
+  before_action :logged_in_user, only: %i[index new create edit update destroy] # ログイン済みユーザーかどうか確認
+  before_action :correct_user_id, only: %i[create update] # リクエストパラメーターのuser_idがログイン中のユーザーのものかどうか確認
+  before_action :correct_users_task, only: %i[edit update destroy] # 正しいユーザーのタスクかどうか確認
 
   # 一覧画面（ログイン中のユーザーのタスク）
   def index
@@ -61,11 +62,17 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :finished, :user_id)
   end
 
-  # 正しいユーザーかどうか確認
-  def correct_user
+  # リクエストパラメータのuser_idがログイン中のユーザーのものかどうか確認
+  def correct_user_id
     if current_user.id != params[:task][:user_id].to_i
       flash[:danger] = 'リクエストが不正です。'
       redirect_to root_url, status: :see_other
     end
+  end
+
+  # 自分のタスク以外は編集・削除できないようにする
+  def correct_users_task
+    @task = Task.find(params[:id])
+    redirect_to(root_url) unless @task.user_id == current_user.id
   end
 end
